a1d54d4099d594ae52cb1b054d90e64c3893381a
Ismael Gomes <ismael.gomes@clinicasim.com>
2022-01-02 15:50:48 -0300

    contact form and data persistence

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bytebank2/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.green[900],
+          secondary: Colors.blueAccent[700],
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
